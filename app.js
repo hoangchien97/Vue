61a6382933cfa24a493599888abbc89ea20bfb8e
Hoang Chien <hoangducchien97@gmail.com>
2019-07-10 14:55:45 +0700

    data binding

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ new Vue({
     data: {
         name: "chien",
         job: "dev",
-        website : 'facebook.com'
+        website : 'http://www.facebook.com',
+        websiteTag : '<a href="https://www.fb.com/hdchien97">Chien Hoang</a>'
     },
     methods: {
         greet: function(time) {
