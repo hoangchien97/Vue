b82df9135904e39f3d1eab531020c74555fd9acb
Hoang Chien <hoangducchien97@gmail.com>
2019-07-10 16:29:13 +0700

    computed properties

--- app.js
+++ app.js
@@ -1,15 +1,73 @@
+// Một phương thức có thể trả về một giá trị hoặc cũng có thể không, 
+// phương thức có thể nhận tham số đầu vào và xử lý. 
+// Phương thức sẽ được gọi mỗi khi component được render 
+
+//  computed giúp cho các component được gọi điều tương tự nhiều lần
+
+/*
+    Sự khác nhau 
+    <template>
+    <div class="my-component">
+        <div>{{ reverseMessage }}</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'this is text'
+            }
+        },
+        computed: {
+            reverseMessage() {
+                return this.message.split('').reverse().join('')
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .my-component {
+        color: red;
+    }
+</style>
+
+Điều gì xảy ra nếu các ở trong cặp dấu {{ }} ở template các bạn viết là reverseMessage() thay vì reverseMessage,
+khi đó lúc chạy ở console sẽ báo lỗi _vm.reverseMessage is not a function.
+Từ đây mình muốn rút ra cho các bạn một số điểm khác biệt giữa computed và methods:
+
+
+    - Khi gọi computed -> ta không được thêm () vào đằng sau
+    - Có nghĩa là computed không nhận tham số đầu vào như methods
+    - computed chỉ nên dùng với dữ liệu có sẵn trong data
+
+    - computed chỉ tính toán lại mỗi khi các biến phụ trong nó thay đổi
+    - methods tính toán lại bất cứ khi nào nó được gọi
+    => tận dụng computed tính toán các dữ liệu có sẵn sẽ cải thiện đc performance
+*/ 
+
 new Vue({
     el: "#app",
     data:{
-        name: '',
-        age: ''
+        age: 23,
+        a: 0,
+        b: 0
     },
-    methods: {
-        logName: function(){
-            console.log('you enter your name');
+    // methods: {
+    //     addToA: function(){
+    //         return this.age + this.a 
+    //     },
+    //     addToB: function(){
+    //         return this.age + this.b;
+    //     }
+    // },
+    computed: {
+        addToA: function(){
+            return this.age + this.a 
         },
-        logAge: function(){
-            console.log('you enter your age');
+        addToB: function(){
+            return this.age + this.b;
         }
     },
 })
