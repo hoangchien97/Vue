acfaeb3397cfcb334ff609045cec384ece791be4
Hoang Chien <hoangducchien97@gmail.com>
2019-07-11 11:57:41 +0700

    component css(scope)

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,50 +1,27 @@
 <template>
-  <div id="app">
-    <h1>{{ title }}</h1>
-    <p>{{greeting()}}</p>
-  </div>
+    <div>
+        <h1>{{ title }}</h1>
+        <ninjas></ninjas>
+    </div>
 </template>
 
 <script>
+// Imports
+import Ninjas from './Ninjas.vue';
 export default {
-  data () {
-    return {
-      title: 'Your first Vue file, woo'
+    components: {
+        'ninjas': Ninjas
+    },
+    data () {
+        return {
+          title: 'Ninja App'
+        }
     }
-  },
-  methods: {
-    greeting:function(){
-      return 'hey cowboy!'
-    }
-  }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style scoped>
+  h1{
+    color: green
+  }
 </style>
---
+++ vuejs-playlist/src/Ninjas.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>List of Ninja</h1>
+        <ul>
+            <li v-for="(ninja,index) in ninjas">{{index}} - {{ ninja }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+          ninjas: ['Yoshi', 'Mario', 'Ryu']
+        }
+    }
+}
+</script>
+
+<style>
+    h1{
+        color: purple
+    }
+</style>
