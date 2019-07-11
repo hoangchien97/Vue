838afd0bd0ce5204562181ae9737d34df8c9ebf3
Hoang Chien <hoangducchien97@gmail.com>
2019-07-11 08:54:13 +0700

    Multiple Vue Intances

--- app.js
+++ app.js
@@ -1,22 +1,32 @@
-new Vue({
-    el: "#app",
+var vm1 = new Vue({
+    el: "#app1",
     data:{
-        health: 100,
-        ended: false       
+          title: "Vue App One" 
     },
     methods: {
-        punch: function(){
-            this.healtah -= 10
-            if(this.health < 0){
-                this.ended = true;
-            }
-        },
-        restart: function(){
-            this.health = 100,
-            this.ended = false
+        
+    },
+    computed: {
+        greet: function(){
+            return 'Hello from app one'
+        }
+    },
+})
+
+var vm2 = new Vue({
+    el: "#app2",
+    data:{
+        title : "Vue App Two"
+    },
+    methods: {
+        changTitle: function(){
+            vm1.title = "Title changed";
         }
     },
     computed: {
+        greet: function(){
+            return 'Yo dudes, this is app 2 speaking to ya :)'
+        }
     },
 })
 
