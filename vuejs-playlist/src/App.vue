8103614dedf6de101c04f2fa520bce8e255fde25
HoangChien <hoangducchien97@gmail.com>
2019-09-27 10:07:55 +0700

    dynamic component

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,44 +1,32 @@
 <template>
     <div>
-        <app-header 
-            :title="title"
-            @changeTitle="updateTitle($event)"
-        ></app-header>
-        <app-ninjas :ninjas="ninjas"></app-ninjas>
-        <app-footer :copyright="copyright" :title="title"></app-footer>
+        <keep-alive>
+            <component :is="currentTabComponent"></component>
+        </keep-alive>
+        <button @click="currentTabComponent = 'form-one'">Show form one</button>
+        <button @click="currentTabComponent = 'form-two'">Show form two</button>
     </div>
 </template>
 
 <script>
 // Imports
-import Header from './components/Header.vue';
-import Footer from './components/Footer.vue';
-import Ninjas from './components/Ninjas.vue';
+import FormOne from './components/FormOne.vue';
+import FormTwo from './components/FormTwo.vue';
+import FormHelper from './components/FormHelper.vue';
 
 export default {
     components: {
-        'app-header': Header,
-        'app-footer': Footer,
-        'app-ninjas': Ninjas
+        FormOne,
+        FormTwo,
+        FormHelper
     },
     data () {
         return {
-          ninjas: [
-            {name: 'Ryu', speciality: 'Vue Components', show: false},
-            {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-            {name: 'Hitoshi', speciality: 'Click Events', show: false},
-            {name: 'Tango', speciality: 'Conditionals', show: false},
-            {name: 'Kami', speciality: 'Webpack', show: false},
-            {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-          ],
-          title: 'Vue Ninjas',
-          copyright: 'Copyright 2019 Chien HD'
+            currentTabComponent: 'form-one'
         }
     },
     methods:{
-        updateTitle: function(updateTitle){
-            this.title = updateTitle;
-        }
+
     }
 }
 </script>
